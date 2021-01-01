9bc4b7f8d8bc77bc056423b93af954a725b49310
yjacks <67487960+yjacks@users.noreply.github.com>
2021-01-01 20:50:38 +0800

    Add files via upload

--- winvi.py
+++ winvi.py
@@ -2,42 +2,13 @@ from os import system, name
 from os.path import exists, abspath
 from sys import exit, argv
 
-system_log=''
+import chardet
+
 echo_mode = False
 file_path = []
 l = []
 l1 = []
 index = -1
-help_text="""
-    欢迎使用新的winvi
-    新的winvi打开二进制文件会直接退出
-    使用帮助：
-    输入:COMMAND进入命令编辑模式：
-        命令用法：
-            :S 保存缓存修改
-            :X 退出
-            :O 切换编辑的文件(默认为第一个命令参数的文件)(保存文件)
-            :OX 强制切换文件
-            :G 将文件现有内容加载至缓存
-            :G1 将目前的修改存放至缓存
-            :R 清空缓存
-            :D 清空文件
-            :ECHO>>FILE 使用命令echo xxx >> file 保存文件(追加式)(请使用完成以后使用:G记录)
-            :PL 输出文件列表
-            :C 清除某一行的全部内容
-            :M 将文件指针移到某行尾(-1为默认)
-            :ADD_OTHERS_FILE 将文件缓存至文件列表
-            :PATH 当前文件路径
-            :U 执行系统命令
-            :PU 显示日志
-            :EXIT_COMMAND,:EC 退出命令模式
-
-    独立指令:
-    :H 输出本帮助（等效－h参数）
-    :P 输出文件内容
-    :PM 输出文件内容(前加序号)
-    :CLEAR_WINDOW,:CW 清屏
-    """
 
 if argv[0] == "python":
     argv.pop(0)
@@ -54,7 +25,34 @@ if argv[1] == "-v":
     print('\t\twinvi version:1.1\n\t\tnew winvi')
     exit()
 elif argv[1] == '-h':
-    print(help_text)
+    print("""
+    欢迎使用新的winvi
+    新的winvi打开二进制文件会直接退出
+    使用帮助：
+    输入:COMMAND或:CMD进入命令编辑模式：
+        命令用法：
+            :S 保存缓存修改
+            :X 退出
+            :O 切换编辑的文件(默认为第一个命令参数的文件)(保存文件)
+            :OX 强制切换文件
+            :G 将文件现有内容加载至缓存
+            :G1 将目前的修改存放至缓存
+            :R 清空缓存
+            :D 清空文件
+            :ECHO>>FILE 使用命令echo xxx >> file 保存文件(追加式)(请使用完成以后使用:G记录)
+            :PL 输出文件列表
+            :C 清除某一行的全部内容
+            :M 将文件指针移到某行尾(-1为默认)
+            :ADD_OTHERS_FILE 将文件缓存至文件列表
+            :PATH 当前文件路径
+            :EXIT_COMMAND 退出命令模式
+            
+    独立指令:
+    :P 输出文件内容
+    :PM 输出文件内容(前加序号)
+    :CLEAR_WINDOW 清屏
+    """)
+    exit()
 
 argv.pop(0)
 if len(argv) > 1:
@@ -64,17 +62,20 @@ else:
     file_path.append(argv[0])
 
 file = file_path[0]
+
 if exists(file):
-    r = open(file, 'r')
+    test_open_file = open(file, 'rb')
+    coding = chardet.detect(test_open_file.read())['encoding']
+    test_open_file.close()
+    r = open(file, 'r', encoding=coding)
     l = r.readlines()
 
-w = open(file, 'w')
-r = open(file, 'r')
+w = open(file, 'w', encoding=coding)
+r = open(file, 'r', encoding=coding)
 
 while True:
     command = input()
     if command == ":COMMAND":
-        print('成功进入COMMAND模式')
         while True:
             cmd = input()
             if cmd == ':S':
@@ -83,7 +84,7 @@ while True:
             elif cmd == ":X":
                 exit()
             elif cmd == ':D':
-                w = open(file, 'w')
+                w = open(file, 'w',encoding=coding)
             elif cmd == ':R':
                 l = []
             elif cmd == ":O":
@@ -103,10 +104,10 @@ while True:
                     file = file_path[use_file]
                     w.writelines(l)
                     if exists(file):
-                        r = open(file, 'r')
+                        r = open(file, 'r',encoding=coding)
                         l = r.readlines()
-                    w = open(file, 'w')
-                    r = open(file, 'r')
+                    w = open(file, 'w',encoding=coding)
+                    r = open(file, 'r',encoding=coding)
                     print("成功切换")
                     continue
             elif cmd == ':OX':
@@ -118,14 +119,13 @@ while True:
                 use_file = int(input())
                 file = file_path[use_file]
                 if exists(file):
-                    r = open(file, 'r')
+                    r = open(file, 'r',encoding=coding)
                     l = r.readlines()
-                w = open(file, 'w')
-                r = open(file, 'r')
+                w = open(file, 'w',encoding=coding)
+                r = open(file, 'r',encoding=coding)
                 print("成功切换")
                 continue
             elif cmd == ':EXIT_COMMAND' or cmd == ':EC':
-                print('退出COMMAND模式')
                 break
             elif cmd == ":PATH":
                 print("当前路径为：" + abspath(file) + "\\" + file)
@@ -168,23 +168,15 @@ while True:
                     l1 = l
             elif cmd == ':PL':
                 print(l)
-            elif cmd == ':U':
-                system_log=system(input('输入指令'))
-            elif cmd == ':PU':
-                print(system_log)
             else:
                 print('输入非指令')
-
-    # 退出COMMAND范畴
-    elif command == ':CLEAR_WINDOW' or command == ':CW':
+    elif command == ':CLEAR_WINDOW':
         if name == 'nt':
             system('cls')
         elif name == 'posix':
             system('clear')
         else:
             print('Java虚拟机(Jython)暂不支持清屏')
-    elif command == ':H':
-        print(help_text)
     elif command == ':P':
         for back_in in l:
             print(back_in)
